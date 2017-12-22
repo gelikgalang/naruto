f950a47bae138b6ab808e765d9083b4b257da640
gelikgalang <gelic.galang.ag@gmail.com>
2017-12-22 13:50:46 +0800

    edited precedence; initial parsing (yung mga p[0] shit)--dami pa errors

--- Source Code/parser.py
+++ Source Code/parser.py
@@ -1,6 +1,9 @@
 import ply.lex as lex
 import sys, os
 import ply.yacc as yacc
+from compiler import ast
+
+#-- List of Tokens --#
 tokens=(
 	'progStart_tok',
 	'progEnd_tok',
@@ -27,30 +30,43 @@ tokens=(
 	'equal_tok',
 	'relationalOp_tok',
 	'not_tok',
-	'plusMinus_tok',
-	'mulDivMod_tok',
+	'plus_tok',
+	'minus_tok',
+	'mult_tok',
+	'div_tok',
+	'mod_tok',
 	'comment',
 	'float_tok',
 	'anything_tok'
 	)
+
+#-- Tokens for program header/footer --#
 t_progStart_tok = r"(@genin)"
 t_progEnd_tok = r"(@kage)"
+
+#-- Tokens for input/output --#
 t_print_tok = r"(@byakugan)"
 t_read_tok = r"(@summon)"
-t_true_tok = r"(@naruto)"
-t_false_tok = r"(@boruto)"
+
+#-- Tokens for loop/if-else statements --#
 t_while_tok=r"(@izanami)"
 t_if_tok=r"(@ifsu)"
 t_else_tok=r"(@elsu)"
 t_end1_tok=r"(@seal)"
 t_for_tok=r"(@mission)"
 t_end2_tok=r"(@end)"
-t_comma_tok = r','
+
+#-- RegEx for Tokens -- #
 t_dataType_tok=r"@number|@decimal|@boolean|@string"
 integer=r'[0-9]+'
 t_num_tok=integer
 t_float_tok=r'('+integer+'\.'+integer+')'
 t_varName_tok = r"([A-Za-z][A-Za-z0-9]*)"
+
+#-- Tokens for symbols --#
+t_true_tok = r"(@naruto)"
+t_false_tok = r"(@boruto)"
+t_comma_tok = r','
 t_lBrace_tok = r'\['
 t_rBrace_tok = r']'
 t_lParen_tok = r'\('
@@ -60,17 +76,24 @@ t_gThan_tok = r'>'
 t_equal_tok=r'='
 t_relationalOp_tok=r"((==)|(>=)|(<=)|(~=))"
 t_not_tok=r'~'
-t_mulDivMod_tok=r'(\*|\/|\%)'
-t_plusMinus_tok=r'(\+|-)'
+t_plus_tok = r'\+'
+t_minus_tok = r'-'
+t_mult_tok = r'\*'
+t_div_tok = r'\/'
+t_mod_tok = r'\%'
+
+
+
+#-- Token to ignore: whitespace --#
 t_ignore  = ' \t'
 
 
 def t_comment(t):
-    r'\!.*'
+    r'\![^\!]*\!'
     return t
     # No return value. Token discarded
 def t_anything_tok(t):
-    r'\".*'
+    r'\"[^\"]*\"'
     return t
     # No return value. Token discarded
 
@@ -84,14 +107,26 @@ def t_error(t):
     t.lexer.skip(1)
 
 
+#-- YACC --#
+precedence = (
+	('nonassoc', 'lParen_tok','rParen_tok','varName_tok','dataType_tok','comment',
+		'if_tok','print_tok','read_tok','num_tok','float_tok','while_tok',
+		'for_tok','rBrace_tok','lBrace_tok','end1_tok','end2_tok'),
+	('left', 'plus_tok','minus_tok'),
+	('left', 'mult_tok','div_tok','mod_tok'),
+)
 
 
-def p_start(t):
+
+def p_start(p):
 	'''start : comments main'''
-def p_main(t):
-	'''main : progStart_tok leftBrace_tok statements rightBrace_tok progEnd_tok'''
+	p[0] = p[1] + p[2]
 
-def p_statements(t):
+def p_main(p):
+	'''main : progStart_tok lBrace_tok statements rBrace_tok progEnd_tok'''
+	p[0] = [p[3]]
+
+def p_statements(p):
 	'''statements : expr statements
 		|	varDec statements
 		|	varAssign statements
@@ -100,84 +135,135 @@ def p_statements(t):
 		|	ifelse statements
 		|	IO statements
 		|	empty'''
-def p_IO(t):
+	if len(p) == 3:
+		
+	else:
+		p[0] = p[1]
+
+def p_IO(p):
 	'''IO :	print_tok lParen_tok IOBody rParen_tok
 		|	read_tok varName_tok varDecArr
 		'''
-def p_IOBody(t):
+	if len(p) == 4:
+		p[0] = p[3]
+
+def p_IOBody(p):
 	'''IOBody : varName_tok varDecArr IOEnd 
 		|	anything_tok IOEnd'''
-def p_IOEnd(t):
-	'''IOEnd : commma_tok IOBody | empty'''
+	if len(p) == 3:
+		p[0] = p[3]
 
-def p_varDecArr(t):
-	'''varDecArr : varArr | empty'''
-def p_varArr(t):			
-	'''varArr : lThan_tok num_tok gThan_tok varName_tok gThan_tok '''
-def p_varAssign(t):
+def p_IOEnd(p):
+	'''IOEnd : comma_tok IOBody 
+		| empty'''
+	if len(p) == 2:
+		p[0] = p[1]
+def p_varDec(p):
+	'''varDec : dataType_tok varName_tok varDecArr varDecBody'''
+
+def p_varDecBody(p):
+	'''varDecBody : comma_tok varName_tok
+		| empty '''
+
+def p_varDecArr(p):
+	'''varDecArr : varArr 
+		| empty'''
+
+def p_varArr(p):			
+	'''varArr : lThan_tok num_tok gThan_tok 
+		| lThan_tok varName_tok gThan_tok '''
+
+def p_varAssign(p):
 	'''varAssign : varName_tok varDecArr equal_tok expr'''
-def p_boolAssign(t):
-	'''boolAssign : true_tok | false_tok'''
-def p_comments(t):
-	'''comments : comment | empty'''
-def p_expr(t):
-	'''expr : expr plusMinus_tok expr 
-		|	term
+
+def p_boolAssign(p):
+	'''boolAssign : true_tok 
+		| false_tok'''
+
+def p_comments(p):
+	'''comments : comment 
+		| empty'''
+
+def p_expr(p):
+	'''expr : expr plus_tok expr
+		|	expr minus_tok expr
+		|	expr mult_tok expr
+		|	expr div_tok expr
+		|	expr mod_tok expr   
+		|	lParen_tok expr rParen_tok
 		|	varName_tok varDecArr
+		|	id
 	'''
-def p_expr2(t):
+
+def p_expr2(p):
 	'''expr2 : expr relationalOp_tok expr
 		|	boolAssign'''
-def p_condition(t):
-	'''condition : expr2 | not_tok condition | num_tok'''
-def p_loops(t):
-	'''loops : while | for'''
-def p_while(t):
-	'''while : while_tok condition statements end1_tok while_tok'''
-def p_for(t):
-	'''for : for_tok varAssign comma_tok num_tok comma_tok num_tok statements end2 for_tok'''
-def p_ifelse(t):
-	'''ifelse : if_tok condition statements end1_tok if_tok elseif'''
-def p_elseif(t):
-	'''elseif : else_tok if_tok condition statements end1_tok else_tok if_tok else | empty '''
-def p_else(t):
-	'''else : else_tok statements | elseif | empty'''
-def p_term(t):
-	'''term: term mulDivMod_tok term | factor'''
-def p_factor(t):
-	'''id | lParen_tok expr rParen_tok'''
-def p_id(t):
-	'''id : num_tok | float_tok '''
-def p_empty(t):
-	'''empty :'''	
 
+def p_condition(p):
+	'''condition : expr2 
+		| not_tok condition 
+		| num_tok'''
+
+def p_loops(p):
+	'''loops : while 
+		| for'''
+
+def p_while(p):
+	'''while : while_tok condition statements end1_tok while_tok'''
+
+def p_for(p):
+	'''for : for_tok varAssign comma_tok num_tok comma_tok num_tok statements end2_tok for_tok'''
+
+def p_ifelse(p):
+	'''ifelse : if_tok condition statements end1_tok if_tok elseif'''
+
+def p_elseif(p):
+	'''elseif : else_tok if_tok condition statements end1_tok else_tok if_tok else 
+		| empty '''
+
+def p_else(p):
+	'''else : else_tok statements 
+		| elseif 
+		| empty'''
+
+def p_id(p):
+	'''id : num_tok 
+		| float_tok '''
+	#p[0] = p[1]
+
+def p_empty(p):
+	'empty :'
+	pass	
+
+def p_error(p):
+	if p:
+		print("Error in line ",p.lineno)
+		print("Syntax error at token",p.type)
+	else:
+		print("Syntax error at EOF! ``@kage'' should be at the last line to end the program properly.")
 
 def startParse():
 	data = []
-	source_name = sys.argv[1]
-	with open(source_name, 'r') as content_file:
+	sourceFile = sys.argv[1]
+	if ".naruto" not in sourceFile:
+		print "Invalid file type!"
+		sys.exit()
+
+	with open(sourceFile, 'r') as content_file:
 	    data = content_file.read()
 
-	lexer = lexy.lex()
+	lexer = lex.lex()
 	lexer.input(data)
 
-	parser = yacc.yacc(debug=False)
+	parser = yacc.yacc()
 	parser.parse(data,tracking=True)
-	# print "Working"
 
-	if debug == 1:
-		toks = []
-		tok1 =""
-		lexer.input(data)
-		while True:
-		    tok = lexer.token()
-		    if not tok: 
-		        break      # No more input
-		    toks.append(tok.value)
-		    print tok
-		    tok1 = tok1+tok.value
-		print toks
-		print tok1
+	# Tokenize
+	while True:
+		tok = lexer.token()
+		if not tok: 
+			break      # No more input
+		print(tok)
 
 	return True
 
